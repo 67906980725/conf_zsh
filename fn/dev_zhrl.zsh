15382ba353d316b0a4fea5dee958960dc3f835db
v <zh1024@zohomail.cn>
2022-11-29 00:21:34 +0800

    update

---
+++ fn/dev_zhrl.zsh
@@ -0,0 +1,4 @@
+vpn_zhrl()
+{
+    sudo openvpn --cd /home/v/.config/openvpn/zhrl --config client.ovpn
+}
--- fn/recreation.zsh
+++ fn/recreation.zsh
@@ -1,5 +1,5 @@
 alias img="mpv --image-display-duration=inf --fs --loop=inf"
-alias imv="mpv --shuffle --border=yes --volume=56 ${MV_DIR}"
+alias imv="mpv --shuffle --border=yes --volume=56 --geometry=100%:0 --autofit=20% --ontop-level=system --ontop ${MV_DIR}"
 alias imusic="mpv --no-video --shuffle --volume=56 ${MV_DIR}"
 alias mg="nvim ."
 alias note="cd ${NOTE_DIR} && mg"
