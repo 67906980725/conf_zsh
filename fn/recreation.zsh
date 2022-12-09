def596506620fed54ed2e8adbd6b984b2c4b4660
v <zh1024@zohomail.cn>
2022-12-09 11:59:06 +0800

    update

--- fn/recreation.zsh
+++ fn/recreation.zsh
@@ -1,5 +1,15 @@
 alias img="mpv --image-display-duration=inf --fs --loop=inf"
 alias imv="mpv --shuffle --border=yes --volume=56 --geometry=100%:0 --autofit=20% --ontop-level=system --ontop ${MV_DIR}"
 alias imusic="mpv --no-video --shuffle --volume=56 ${MV_DIR}"
-alias mg="nvim ."
+
+mg()
+{
+  if [ "$1" = "" ]; then
+      nvim .
+  else
+      z "$1"
+      nvim .
+  fi
+}
+
 alias note="cd ${NOTE_DIR} && mg"
